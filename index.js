bb3138e0e21b7e8af71c36923235f7ecf6e4a9c3
xdundun <xdundun@gmail.com>
2020-05-18 13:44:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(sourceArray, fn) {
+  let newArray = []
+
+  for(let i = 0; i < sourceArray.length; i++) {
+    newArray[i] = (fn(sourceArray[i]))
+  }
+  return newArray
+}
+
+function reduce(sourceArray, fn, start) {
+  let newArray = (!!start) ? start : sourceArray[0]
+  let i = (!!start) ? 0 : 1
+
+  for(; i < sourceArray.length; i++) {
+    newArray = fn(sourceArray[i], newArray)
+  }
+  return newArray
+}
